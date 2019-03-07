87f3b4f8285e12e6bc90d20ee0658f592447564a
x.casafont <x.casafont@DESKTOP-86ORE9B>
2019-03-07 15:32:11 +0100

    commit all project solutions

---
+++ APILayer.Client/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace APILayer.Client
+{
+    public class Class1
+    {
+    }
+}
---
+++ APILayer.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace APILayer.Entities
+{
+    public class Class1
+    {
+    }
+}
