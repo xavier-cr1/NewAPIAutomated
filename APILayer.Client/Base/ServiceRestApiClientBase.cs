99b07fef5a36025f9eda75439e83931c5cb40dda
x.casafont <x.casafont@DESKTOP-86ORE9B>
2019-03-08 14:08:36 +0100

    adding setting files

---
+++ APILayer.Client/Base/ServiceRestApiClientBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APILayer.Client.Base
+{
+    public class ServiceRestApiClientBase
+    {
+        protected readonly string fromDateBaseAttr = "todate=";
+
+        protected readonly string toDateBaseAttr = "todate=";
+
+
+        protected readonly IConfigurationRoot configurationRoot;
+
+        //Inject configuration json file into rest service base
+        public ServiceRestApiClientBase(IConfigurationRoot configurationRoot)
+        {
+            this.configurationRoot = configurationRoot;
+        }
+
+        //authenticiy management if needed
+
+        //swagger configs...
+    }
+}
--- APILayer.Client/PostsServiceRestApi.cs
+++ APILayer.Client/PostsServiceRestApi.cs
@@ -1,12 +1,21 @@
-﻿using APILayer.Client.Contracts;
+﻿using APILayer.Client.Base;
+using APILayer.Client.Contracts;
 using APILayer.Entities.PostsService;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
 
 namespace APILayer.Client
 {
-    public class PostsServiceRestApi : IPostsServiceRestApi
+    public class PostsServiceRestApi : ServiceRestApiClientBase, IPostsServiceRestApi
     {
+        private string postsService => this.configurationRoot.GetSection("AppConfiguration")["PostsAPIService"];
+
+        public PostsServiceRestApi(IConfigurationRoot configurationRoot) 
+            : base (configurationRoot)
+        {
+        }
+
         public Task<PostsRootResponse> PostsServiceGetAsync(PostsRequest postsRequest)
         {
             throw new NotImplementedException();
